b630e4a309f326d1cd770c5feb95b9671b36f2cf
Jmint <jeffreymintah737@gmail.com>
2024-05-03 22:18:45 +0000

    changes

--- weather_app/lib/home_screen.dart
+++ weather_app/lib/home_screen.dart
@@ -36,7 +36,7 @@ class HomeScreen extends StatelessWidget {
       body: const Column(
         children: [
           Placeholder(
-            fallbackHeight: 50,
+            fallbackHeight: 250,
           )
         ],
       ),
