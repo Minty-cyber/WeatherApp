cc12807dfec4f0e1c075a7333a60eb0e48d9d961
Jmint <jeffreymintah737@gmail.com>
2024-05-03 21:06:44 +0000

    changes

--- weather_app/lib/home_screen.dart
+++ weather_app/lib/home_screen.dart
@@ -8,7 +8,7 @@ class HomeScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar: AppBar(
-        title: Text('Weather App'),
+        title: const Text('Weather App'),
       ),
     );
   }
