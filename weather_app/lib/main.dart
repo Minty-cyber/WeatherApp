0bcbf38ef916756d054109484536540a4a5f61de
Jmint <jeffreymintah737@gmail.com>
2024-05-03 20:52:11 +0000

    changes

--- weather_app/lib/main.dart
+++ weather_app/lib/main.dart
@@ -12,6 +12,7 @@ class MyWeatherApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: flase,
       home: HomeScreen(),
     );
   }
