0637f6e0855d58cc7ea4820988fd2c2d19828d7b
Jmint <jeffreymintah737@gmail.com>
2024-05-04 22:28:59 +0000

    changes

--- weather_app/lib/home_screen.dart
+++ weather_app/lib/home_screen.dart
@@ -95,179 +95,181 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
             const SizedBox(height: 16,),
-            Row(
-              children: [
-              Card(
-                elevation: 6,
-                child: Container(
-                  width: 100,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  padding: const EdgeInsets.all(8.0),
-                  child: const Column(
-                    children: [
-                      Text(
-                        '9:00',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
+            SingleChildScrollView(
+              child: Row(
+                children: [
+                Card(
+                  elevation: 6,
+                  child: Container(
+                    width: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    padding: const EdgeInsets.all(8.0),
+                    child: const Column(
+                      children: [
+                        Text(
+                          '9:00',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                      SizedBox(height: 8),
-                      Icon(
-                        Icons.cloud,
-                        size: 32,
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        '300.21',
-                        style: TextStyle(
-                          // fontSize: 16,
+                        SizedBox(height: 8),
+                        Icon(
+                          Icons.cloud,
+                          size: 32,
                         ),
-                      ),
-                  
-                    ],
-                  ),
-                ),
-              ),
-              Card(
-                elevation: 6,
-                child: Container(
-                  width: 100,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  padding: const EdgeInsets.all(8.0),
-                  child: const Column(
-                    children: [
-                      Text(
-                        '9:00',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
+                        SizedBox(height: 8),
+                        Text(
+                          '300.21',
+                          style: TextStyle(
+                            // fontSize: 16,
+                          ),
                         ),
-                      ),
-                      SizedBox(height: 8),
-                      Icon(
-                        Icons.cloud,
-                        size: 32,
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        '300.21',
-                        style: TextStyle(
-                          // fontSize: 16,
-                        ),
-                      ),
-                  
-                    ],
+                    
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              Card(
-                elevation: 6,
-                child: Container(
-                  width: 100,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  padding: const EdgeInsets.all(8.0),
-                  child: const Column(
-                    children: [
-                      Text(
-                        '9:00',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
+                Card(
+                  elevation: 6,
+                  child: Container(
+                    width: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    padding: const EdgeInsets.all(8.0),
+                    child: const Column(
+                      children: [
+                        Text(
+                          '9:00',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                      SizedBox(height: 8),
-                      Icon(
-                        Icons.cloud,
-                        size: 32,
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        '300.21',
-                        style: TextStyle(
-                          // fontSize: 16,
+                        SizedBox(height: 8),
+                        Icon(
+                          Icons.cloud,
+                          size: 32,
                         ),
-                      ),
-                  
-                    ],
-                  ),
-                ),
-              ),
-              Card(
-                elevation: 6,
-                child: Container(
-                  width: 100,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  padding: const EdgeInsets.all(8.0),
-                  child: const Column(
-                    children: [
-                      Text(
-                        '9:00',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
+                        SizedBox(height: 8),
+                        Text(
+                          '300.21',
+                          style: TextStyle(
+                            // fontSize: 16,
+                          ),
                         ),
-                      ),
-                      SizedBox(height: 8),
-                      Icon(
-                        Icons.cloud,
-                        size: 32,
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        '300.21',
-                        style: TextStyle(
-                          // fontSize: 16,
-                        ),
-                      ),
-                  
-                    ],
+                    
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              Card(
-                elevation: 6,
-                child: Container(
-                  width: 100,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  padding: const EdgeInsets.all(8.0),
-                  child: const Column(
-                    children: [
-                      Text(
-                        '9:00',
-                        style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
+                Card(
+                  elevation: 6,
+                  child: Container(
+                    width: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    padding: const EdgeInsets.all(8.0),
+                    child: const Column(
+                      children: [
+                        Text(
+                          '9:00',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                      SizedBox(height: 8),
-                      Icon(
-                        Icons.cloud,
-                        size: 32,
-                      ),
-                      SizedBox(height: 8),
-                      Text(
-                        '300.21',
-                        style: TextStyle(
-                          // fontSize: 16,
+                        SizedBox(height: 8),
+                        Icon(
+                          Icons.cloud,
+                          size: 32,
                         ),
-                      ),
-                  
-                    ],
+                        SizedBox(height: 8),
+                        Text(
+                          '300.21',
+                          style: TextStyle(
+                            // fontSize: 16,
+                          ),
+                        ),
+                    
+                      ],
+                    ),
                   ),
                 ),
+                Card(
+                  elevation: 6,
+                  child: Container(
+                    width: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    padding: const EdgeInsets.all(8.0),
+                    child: const Column(
+                      children: [
+                        Text(
+                          '9:00',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 8),
+                        Icon(
+                          Icons.cloud,
+                          size: 32,
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          '300.21',
+                          style: TextStyle(
+                            // fontSize: 16,
+                          ),
+                        ),
+                    
+                      ],
+                    ),
+                  ),
+                ),
+                Card(
+                  elevation: 6,
+                  child: Container(
+                    width: 100,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    padding: const EdgeInsets.all(8.0),
+                    child: const Column(
+                      children: [
+                        Text(
+                          '9:00',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(height: 8),
+                        Icon(
+                          Icons.cloud,
+                          size: 32,
+                        ),
+                        SizedBox(height: 8),
+                        Text(
+                          '300.21',
+                          style: TextStyle(
+                            // fontSize: 16,
+                          ),
+                        ),
+                    
+                      ],
+                    ),
+                  ),
+                ),
+                ],
               ),
-              ],
             ),
             
             const Placeholder(
