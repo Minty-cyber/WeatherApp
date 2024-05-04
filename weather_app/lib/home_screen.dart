abc8582213edc3244794556bced7eebe0c59670f
Jmint <jeffreymintah737@gmail.com>
2024-05-04 20:30:28 +0000

    chnages

--- weather_app/lib/home_screen.dart
+++ weather_app/lib/home_screen.dart
@@ -41,7 +41,7 @@ class HomeScreen extends StatelessWidget {
               width: double.infinity,
               child: Card(
                 shape: RoundedRectangleBorder(
-                  
+                  borderRadius: 
                 ),
                 elevation: 16,
                 child: Padding(
