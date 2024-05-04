ea431e484e7d24e201be4afc1af7b3331c12b967
Jmint <jeffreymintah737@gmail.com>
2024-05-04 22:25:09 +0000

    changes

--- weather_app/lib/home_screen.dart
+++ weather_app/lib/home_screen.dart
@@ -131,6 +131,40 @@ class HomeScreen extends StatelessWidget {
                   ),
                 ),
               ),
+              Card(
+                elevation: 6,
+                child: Container(
+                  width: 100,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  padding: const EdgeInsets.all(8.0),
+                  child: const Column(
+                    children: [
+                      Text(
+                        '9:00',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(height: 8),
+                      Icon(
+                        Icons.cloud,
+                        size: 32,
+                      ),
+                      SizedBox(height: 8),
+                      Text(
+                        '300.21',
+                        style: TextStyle(
+                          // fontSize: 16,
+                        ),
+                      ),
+                  
+                    ],
+                  ),
+                ),
+              ),
               ],
             ),
             
