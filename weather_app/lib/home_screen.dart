337796de7018b8e272df8911e171edae3f51b638
Jmint <jeffreymintah737@gmail.com>
2024-05-04 19:56:13 +0000

    changes

--- weather_app/lib/home_screen.dart
+++ weather_app/lib/home_screen.dart
@@ -55,7 +55,7 @@ class HomeScreen extends StatelessWidget {
                         ,
                         ),
                         Text(
-                          
+                          'Rain'
                         )
                     
                   ],
