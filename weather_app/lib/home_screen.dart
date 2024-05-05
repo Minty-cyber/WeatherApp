f1d5d524cb1987f1d42d4c4f1af1bb6ebba144f5
Jmint <jeffreymintah737@gmail.com>
2024-05-05 08:49:36 +0000

    changes

--- weather_app/lib/home_screen.dart
+++ weather_app/lib/home_screen.dart
@@ -124,6 +124,7 @@ class HomeScreen extends StatelessWidget {
             ),
             const SizedBox(height: 16,),
             const Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
                 AdditionalInfoWidget(),
                 AdditionalInfoWidget(),
