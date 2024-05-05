a21eed9b350e04d329462b5a5ff97f4010073b75
Jmint <jeffreymintah737@gmail.com>
2024-05-05 08:44:37 +0000

    changes

--- weather_app/lib/home_screen.dart
+++ weather_app/lib/home_screen.dart
@@ -133,37 +133,3 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
-
-class AdditionalInfoWidget extends StatelessWidget {
-  const AdditionalInfoWidget({
-    super.key,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return const Column(
-      children: [
-        Icon(
-          Icons.water_drop,
-          size: 32,
-          ),
-          SizedBox(height: 8,),
-          Text(
-            'Humidity',
-            style: TextStyle(
-              // fontSize: 20,
-            ),
-          ),
-          SizedBox(height: 8,),
-          Text(
-            '321.23',
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold
-            ),
-          )
-    
-      ],
-    );
-  }
-}
