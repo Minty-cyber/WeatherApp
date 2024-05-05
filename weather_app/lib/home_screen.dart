bb3a32df7d0e77013210c957601323a7f9dc102a
Jmint <jeffreymintah737@gmail.com>
2024-05-05 09:23:25 +0000

    changes

--- weather_app/lib/home_screen.dart
+++ weather_app/lib/home_screen.dart
@@ -123,37 +123,40 @@ class HomeScreen extends StatelessWidget {
               ),
             ),
             const SizedBox(height: 16,),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                AdditionalInfoWidget(
-                  icon: Icons.water_drop,
-                  label: 'Hunidity',
-                  value: '91',
-                ),
-                AdditionalInfoWidget(
-                  icon: Icons.beach_access_outlined,
-                  label: 'Pressure',
-                  value: '21',
-                ),
-                AdditionalInfoWidget(
-                  icon: Icons.air_sharp,
-                  label: 'Wind Speed',
-                  value: '32.1',
-                ),
-                AdditionalInfoWidget(
-                  icon: Icons.air_sharp,
-                  label: 'Wind Speed',
-                  value: '32.1',
-                ),
-                AdditionalInfoWidget(
-                  icon: Icons.air_sharp,
-                  label: 'Wind Speed',
-                  value: '32.1',
-                ),
-                
-            
-              ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  AdditionalInfoWidget(
+                    icon: Icons.water_drop,
+                    label: 'Hunidity',
+                    value: '91',
+                  ),
+                  AdditionalInfoWidget(
+                    icon: Icons.beach_access_outlined,
+                    label: 'Pressure',
+                    value: '21',
+                  ),
+                  AdditionalInfoWidget(
+                    icon: Icons.air_sharp,
+                    label: 'Wind Speed',
+                    value: '32.1',
+                  ),
+                  AdditionalInfoWidget(
+                    icon: Icons.air_sharp,
+                    label: 'Wind Speed',
+                    value: '32.1',
+                  ),
+                  AdditionalInfoWidget(
+                    icon: Icons.air_sharp,
+                    label: 'Wind Speed',
+                    value: '32.1',
+                  ),
+                  
+              
+                ],
+              ),
             )
           ],
         ),
