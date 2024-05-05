8c0b4b02e4522554b55268c072ffb5316a90c9f0
Jmint <jeffreymintah737@gmail.com>
2024-05-05 13:17:33 +0000

    changes

--- weather_app/lib/home_screen.dart
+++ weather_app/lib/home_screen.dart
@@ -92,7 +92,7 @@ class _HomeScreenState extends State<HomeScreen> {
                 }
 
                 if (snapshot.hasError) {
-                  return const Text(data)
+                  return const Text(snapshot.error)
                 }
                 return SizedBox(
                 width: double.infinity,
