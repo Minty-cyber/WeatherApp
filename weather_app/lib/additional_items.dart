6db3ec781992ba2a0456543b8de74d64022ea2e4
Jmint <jeffreymintah737@gmail.com>
2024-05-05 08:44:46 +0000

    changes

--- weather_app/lib/additional_items.dart
+++ weather_app/lib/additional_items.dart
@@ -1,2 +1,36 @@
 import 'package:flutter/material.dart';
 
+
+class AdditionalInfoWidget extends StatelessWidget {
+  const AdditionalInfoWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Column(
+      children: [
+        Icon(
+          Icons.water_drop,
+          size: 32,
+          ),
+          SizedBox(height: 8,),
+          Text(
+            'Humidity',
+            style: TextStyle(
+              // fontSize: 20,
+            ),
+          ),
+          SizedBox(height: 8,),
+          Text(
+            '321.23',
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.bold
+            ),
+          )
+    
+      ],
+    );
+  }
+}
