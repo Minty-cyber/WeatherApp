ec491a7418d6d34ee4284be6211ac94ffa59f653
Jmint <jeffreymintah737@gmail.com>
2024-05-05 08:38:38 +0000

    changes

--- weather_app/lib/home_screen.dart
+++ weather_app/lib/home_screen.dart
@@ -1,5 +1,5 @@
 import 'dart:ui';
-
+import 'package:weather_app/weather_items.dart';
 import 'package:flutter/material.dart';
 
 
@@ -154,47 +154,5 @@ class HomeScreen extends StatelessWidget {
         ),
       ),
     );
-  }
-}
-
-class HourlyForecastWidget extends StatelessWidget {
-  const HourlyForecastWidget({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-                  elevation: 6,
-                  child: Container(
-                    width: 100,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    padding: const EdgeInsets.all(8.0),
-                    child: const Column(
-                      children: [
-                        Text(
-                          '9:00',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(height: 8),
-                        Icon(
-                          Icons.cloud,
-                          size: 32,
-                        ),
-                        SizedBox(height: 8),
-                        Text(
-                          '300.21',
-                          style: TextStyle(
-                            // fontSize: 16,
-                          ),
-                        ),
-                    
-                      ],
-                    ),
-                  ),
-                );
   }
 }
