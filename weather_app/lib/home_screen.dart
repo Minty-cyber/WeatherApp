7a646bc124beda784c89d1be0751d2972fe644cf
Jmint <jeffreymintah737@gmail.com>
2024-05-05 10:29:27 +0000

    changes

--- weather_app/lib/home_screen.dart
+++ weather_app/lib/home_screen.dart
@@ -12,7 +12,7 @@ class HomeScreen extends StatelessWidget {
   Future getCurrentWeather() async {
     String city = 'London';
     http.get(
-      Uri.parse('https://api.openweathermap.org/data/2.5/weather?q=$city,')
+      Uri.parse('https://api.openweathermap.org/data/2.5/weather?q=$city&APPID')
       );
   }
 
