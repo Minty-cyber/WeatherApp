956433bb7eb6ddd4399d184913c9606c0175713e
Jmint <jeffreymintah737@gmail.com>
2024-05-05 08:07:59 +0000

    changes

--- weather_app/lib/home_screen.dart
+++ weather_app/lib/home_screen.dart
@@ -99,40 +99,7 @@ class HomeScreen extends StatelessWidget {
               scrollDirection: Axis.horizontal,
               child: Row(
                 children: [
-                Card(
-                  elevation: 6,
-                  child: Container(
-                    width: 100,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
-                    padding: const EdgeInsets.all(8.0),
-                    child: const Column(
-                      children: [
-                        Text(
-                          '9:00',
-                          style: TextStyle(
-                            fontSize: 16,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(height: 8),
-                        Icon(
-                          Icons.cloud,
-                          size: 32,
-                        ),
-                        SizedBox(height: 8),
-                        Text(
-                          '300.21',
-                          style: TextStyle(
-                            // fontSize: 16,
-                          ),
-                        ),
-                    
-                      ],
-                    ),
-                  ),
-                ),
+               
                 Card(
                   elevation: 6,
                   child: Container(
