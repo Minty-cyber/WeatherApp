59bc5f5507b35b47d6c5f586b1474087a977c86d
Jmint <jeffreymintah737@gmail.com>
2024-05-05 10:09:22 +0000

    changes

--- weather_app/lib/home_screen.dart
+++ weather_app/lib/home_screen.dart
@@ -10,6 +10,7 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
+      backgroundColor: Colors.amber,
       appBar: AppBar(
         title: const Text(
           'Weather App',
