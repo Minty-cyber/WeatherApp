c620e5e3a36e8b0576846ed1beb2eb6f607144ad
Jmint <jeffreymintah737@gmail.com>
2024-05-05 08:13:34 +0000

    changes

--- weather_app/lib/home_screen.dart
+++ weather_app/lib/home_screen.dart
@@ -103,6 +103,8 @@ class HomeScreen extends StatelessWidget {
                 HourlyForecastWidget(),
                 HourlyForecastWidget(),
                 HourlyForecastWidget(),
+                HourlyForecastWidget(),
+
 
                 
                 ],
